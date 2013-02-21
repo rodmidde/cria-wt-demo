b16835c7b508dbff74d86d4f20f5c4dcef528d99
Rody Middelkoop <rody.middelkoop@han.nl>
2013-02-21 12:04:05 +0100

    Contains highly experimental coding during class

--- MAVEN/DemoApp/src/main/javascript/quickstart.js
+++ MAVEN/DemoApp/src/main/javascript/quickstart.js
@@ -7,3 +7,36 @@
 function addNumbers(number1, number2) {
     return number1 + number2;
 }
+
+/**
+ * Determines the number of rows of columns given a size of photo's
+ * @param numberOfPhotos
+ * @return {Object}
+ */
+function determineRaster(numberOfPhotos) {
+    var sqrtOfNumberOfPhotos = Math.sqrt(numberOfPhotos);
+    var roundedSqrtOfNumberOfPhotos = Math.round(sqrtOfNumberOfPhotos);
+    var restOfRoundenSqrtOfNumberOfPhotos = numberOfPhotos % roundedSqrtOfNumberOfPhotos;
+
+    return {aantalRijen:roundedSqrtOfNumberOfPhotos, aantalKolommen:roundedSqrtOfNumberOfPhotos+(Number)(restOfRoundenSqrtOfNumberOfPhotos > 0)};
+}
+
+function alertElements(list)
+{
+    for(elementIndex in list) {
+        alert(list[elementIndex]);
+    }
+}
+
+var persoon = {
+    naam: "Rody",
+    leeftijd: 37,
+    kinderen: new Array(),
+
+    toonNaam : function()
+    {
+        alert(this.naam)
+    }
+}
+
+
--- MAVEN/DemoApp/src/test/javascript/quickstartTest.js
+++ MAVEN/DemoApp/src/test/javascript/quickstartTest.js
@@ -7,4 +7,31 @@ describe('The add functions',function(){
         var result = addNumbers(number1, number2);
         expect(result).toBe(5);
     });
+
+    it('should return zero when the photolist is empty', function() {
+
+        var expectedResult = {aantalRijen:0, aantalKolommen:0};
+
+        var result = determineRaster(0);
+        expect(result).toEqual(expectedResult);
+    });
+
+    it('should return a raster 1x1 when only one photo available', function() {
+
+        var expectedResult = {aantalRijen:1, aantalKolommen:1};
+
+        var result = determineRaster(1);
+        expect(result).toEqual(expectedResult);
+    });
+
+    /*
+    it('should return a raster 1x1 when only one photo available', function() {
+
+        var expectedResult = {aantalRijen:2, aantalKolommen:1};
+
+        var result = determineRaster(2);
+        expect(result).toEqual(expectedResult);
+    });
+    */
+
 });
