63c450b8dec9d8fb46a5249e2bf0712a6d151e82
Rody Middelkoop <rody.middelkoop@han.nl>
2013-03-14 01:01:47 +0100

    Added JSONP comments

--- AJAX/3 - cors/jquery/script/getImages.js
+++ AJAX/3 - cors/jquery/script/getImages.js
@@ -7,6 +7,12 @@ $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?
         tags: "thetis-pupillen",
         tagmode: "any",
         format: "json"
+        /**
+         * Other URLs may require one or more of the following extra parameters:
+         *   dataType: "jsonp",
+         *   jsonp: "callback",
+         *   jsonpCallback: "cbfunc",
+         */
     },
     function (data) {
         $.each(data.items, function (i, item) {
