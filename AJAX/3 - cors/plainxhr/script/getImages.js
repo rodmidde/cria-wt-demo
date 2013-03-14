ac855cba35994252038e1574579d21c6146b94a1
Rody Middelkoop <rody.middelkoop@han.nl>
2013-03-14 01:49:10 +0100

    Extra proof that the Origin header is key in CORS

--- AJAX/3 - cors/plainxhr/script/getImages.js
+++ AJAX/3 - cors/plainxhr/script/getImages.js
@@ -55,11 +55,17 @@ function handleCorsRequest(data)
 }
 // We're not allowed to set the Origin header manually, this is done by the
 // browser so make sure to run from localhost and not local file
+//
+// FROM https://bugs.webkit.org/show_bug.cgi?id=50773:
+// We don't usually send Origin with GET requests.  We do send it for XMLHttpRequest if we're using CORS.
+// Maybe we're not using CORS for some reason?
+
 // request.setRequestHeader('Origin', 'http://www.han.nl');
 
 // FLICKR (but also YAHOO) does not support requests coming from localhost, bad luck
 request.setRequestHeader('Access-Control-Allow-Origin','*');
 request.setRequestHeader('Access-Control-Allow-Methods','GET');
+
 // YAHOO supports the OpenSocial function to do this http://developer.yahoo.com/yql/guide/yql-code-examples.html#yql_javascript
 // Alternatively, use jQuery with JSONP or use Chrome with --disable-web-security option
 
